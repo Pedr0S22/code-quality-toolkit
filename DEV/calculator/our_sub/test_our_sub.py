2a934b0739db117ea4bf34ceb73dc7c0953b958d
@CarlosLomelinV <uc2025154763@student.uc.pt>
2025-10-14 12:28:48 -0600

    Test 1 realizado y modificado test 4

--- DEV/calculator/our_sub/test_our_sub.py
+++ DEV/calculator/our_sub/test_our_sub.py
@@ -1,5 +1,8 @@
 import pytest
 from our_sub import our_sub
 
-def test_sub_equal_number(a,b):
-    assert our_sub(2, 3 ) == 0, "Should be 0 always"
+def test_sub_equal_trivialsubstraction():
+    assert our_sub(5,2) == 3, "Should be 3 always"
+
+def test_sub_equal_number():
+    assert our_sub(3, 3 ) == 0, "Should be 0 always"
