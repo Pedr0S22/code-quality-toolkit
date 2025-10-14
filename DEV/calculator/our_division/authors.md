94b696639ef7f1055b02c16f37b2da7562f6e09b
André Silva <uc2023212648@student.uc.pt>
2025-10-14 20:11:36 +0100

    autoritario colocado

--- DEV/calculator/our_division/authors.md
+++ DEV/calculator/our_division/authors.md
@@ -1,1 +1,2 @@
 2023218767  @my3007sunshine  my3007sunshine@gmail.com 
+2023212648  @andresilva219  uc2023212648@student.uc.pt
--- DEV/calculator/our_division/test_our_division.py
+++ DEV/calculator/our_division/test_our_division.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 def test_trivial_division():
     assert our_division(4,2)==2, "Should be 2"
-=======
 def test_for_negatives();
     assert our_division(-6,-3) == 2, "Should be 2"
->>>>>>> 2c11f5a110f1adfda9e786fb2445155d9a3abac3
