e797008d24487f40eb5c8c88759ff9b21c80ab92
vascoalvesxyz <true.vasco.alves@gmail.com>
2025-10-14 19:08:32 +0100

    added pytest paramatrize test multiple (commented)

--- DEV/calculator/our_module/test_our_mod.py
+++ DEV/calculator/our_module/test_our_mod.py
@@ -5,3 +5,10 @@ def test_float():
     assert our_module(10, 2.5 ) == 0, "Should be 0"
     assert our_module(33.5, 5 ) == 3.5, "Should be 3.5"
     assert our_module(55.5, 5.5 ) == 0.5, "Should be 0.5"
+
+# @pytest.mark.parametrize(
+#     "a, b, expected",
+#     [ (0, 3.5, 0.5), (10, 2.5, 0), (33.5, 5, 3.5), (55.5, 5.5, 0.5) ]
+# )
+# def test_multiples(a, b, expected):
+#     assert our_module(a, b) == expected
