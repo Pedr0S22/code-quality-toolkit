52e910c97b9cbe7e1f2e2971cd727f2b192dd21c
alfooonsoo17 <apintadon@alumnos.unex.es>
2025-10-14 19:47:51 +0100

    test 2

--- DEV/calculator/our_sub/test_our_sub.py
+++ DEV/calculator/our_sub/test_our_sub.py
@@ -4,5 +4,8 @@ from our_sub import our_sub
 def test_sub_equal_trivialsubstraction():
     assert our_sub(5,2) == 3, "Should be 3 always"
 
+def test_sub_negative_number():
+    assert our_sub(5, -3) == 8, "Should be 8 always"
+
 def test_sub_equal_number():
     assert our_sub(3, 3 ) == 0, "Should be 0 always"
