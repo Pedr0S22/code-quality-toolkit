92874b98c0f791d882fb8d7bbb8c7dbdd5c40281
AugustoBrynne <abhungua@student.dei.uc.pt>
2025-10-15 08:34:43 +0100

    Solved our_add error, ALL TESTS PASSED

--- DEV/calculator/our_module/test_our_mod.py
+++ DEV/calculator/our_module/test_our_mod.py
@@ -1,6 +1,7 @@
 import pytest
 from our_module import our_module
 
+#Our_module test functions to check its behavior with int and float inputs
 def test_float():
     assert our_module(10, 2.5 ) == 0, "Should be 0"
     assert our_module(33.5, 5 ) == 3.5, "Should be 3.5"
