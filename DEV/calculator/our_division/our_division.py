05bec11471d616945f69538d37c5b9addc7cbd7f
Pedro Silva <uc2023235452@student.uc.pt>
2025-10-17 17:20:34 +0100

    changed error message

--- DEV/calculator/our_division/our_division.py
+++ DEV/calculator/our_division/our_division.py
@@ -2,7 +2,7 @@ def our_division(a,b):
     if b!=0:
         return a / b
     else:
-        print("Impossivel fazer divisao")
+        print("Impossible: division by zero error")
         return None
 
 
