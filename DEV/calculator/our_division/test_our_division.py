79e0ee003143e339e7c153bdce847bd74abdfed1
rferyals <uc2024187186@student.uc.pt>
2025-10-17 23:57:36 +0100

    fixed the test by value change

--- DEV/calculator/our_division/test_our_division.py
+++ DEV/calculator/our_division/test_our_division.py
@@ -4,9 +4,9 @@ from our_add import our_add
 def test_trivial_division():
     assert our_division(4,2)==2, "Should be 2"
 =======
-def test_for_negatives();
+def test_for_negatives():
     assert our_division(-6,-3) == 2, "Should be 2"
 >>>>>>> 2c11f5a110f1adfda9e786fb2445155d9a3abac3
 
 def test_for_mixed():
-    assert our_division(2, 0) == 0, "Should be 1 (error)"
+    assert our_division(2, -2) == 1, "Should be 1 (error)"
