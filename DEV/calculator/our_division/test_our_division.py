2be8be6e2b34bade625a8d58b1beb8f74c8f2358
Pedro Silva <uc2023235452@student.uc.pt>
2025-10-17 11:26:16 +0100

    merge conflict resolved; our_division function name changed

--- DEV/calculator/our_division/test_our_division.py
+++ DEV/calculator/our_division/test_our_division.py
@@ -1,12 +1,11 @@
-<<<<<<< HEAD
 import pytest
-from our_add import our_add
+from our_division import our_division
+
 def test_trivial_division():
     assert our_division(4,2)==2, "Should be 2"
-=======
-def test_for_negatives();
+    
+def test_for_negatives():
     assert our_division(-6,-3) == 2, "Should be 2"
->>>>>>> 2c11f5a110f1adfda9e786fb2445155d9a3abac3
 
 def test_for_mixed():
     assert our_division(2, 0) == 0, "Should be 1 (error)"
