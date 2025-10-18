140ebc0f163be2ce898ee63229dc3438f990a111
Pedro Silva <uc2023235452@student.uc.pt>
2025-10-18 11:22:35 +0100

    Removed Rabia'a commits (since it deleted @Pedr0S22 and @Isaque_capra work) and updated division_by_zero test with a «should...» message

--- DEV/calculator/our_division/test_our_division.py
+++ DEV/calculator/our_division/test_our_division.py
@@ -7,7 +7,7 @@ def test_for_negatives():
     assert our_division(-6,-3) == 2, "Should be 2"
 
 def test_for_mixed():
-    assert our_division(2, 0) == 0, "Should be 1 (error)"
+    assert our_division(2, -2) == -1, "Should be -1"
 
 def test_division_by_floats():
     assert our_division(5.0, 0.5) == 10.0, "Should be 10.0"
@@ -16,4 +16,4 @@ def test_division_by_zero():
     with pytest.raises(ZeroDivisionError) as ex:
         our_division(12, 0)
 
-    assert str(ex.value) == "division by zero error"
+    assert str(ex.value) == "division by zero error", "should give a «division by zero error» message"
