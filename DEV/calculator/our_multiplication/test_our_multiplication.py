77b75155d6f494eb07bc54a26ce483ee5144265d
delvivo.diogo1 <delvivo.diogo1@gmail.com>
2025-10-18 22:55:50 +0100

    changed the test_for_zero for merging purposes

--- DEV/calculator/our_multiplication/test_our_multiplication.py
+++ DEV/calculator/our_multiplication/test_our_multiplication.py
@@ -5,7 +5,7 @@ def test_trivial_multiplication():
     assert our_multiplication(2, 3) == 6, "Should be 6"
 
 def test_for_zero():
-    assert our_multiplication(5, 0) == 1, "Should be 0 (error)"
+    assert our_multiplication(5, 0) == 0, "Should be 0 (error)"
 
 def test_for_negatives():
     assert our_multiplication(-2, 3) == -6, "Should be -6"
