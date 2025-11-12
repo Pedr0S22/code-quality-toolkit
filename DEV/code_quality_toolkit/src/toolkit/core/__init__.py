38245c4aeabca02f4ae9a52f678ae87421a01e87
Pedro Silva <uc2023235452@student.uc.pt>
2025-11-12 11:47:54 +0000

    lint correction on src/toolkit/core/__init__.py

--- DEV/code_quality_toolkit/src/toolkit/core/__init__.py
+++ DEV/code_quality_toolkit/src/toolkit/core/__init__.py
@@ -3,7 +3,7 @@
 
 import typing as _t
 if _t.TYPE_CHECKING:
-    from . import cli as _cli  # for type checkers only
+    from . import cli as _cli  # for type checkers only # noqa: F401
 
 def __getattr__(name):
     if name == "cli":
