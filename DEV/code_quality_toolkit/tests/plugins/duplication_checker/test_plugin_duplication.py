4bc111d68dde6185ed75e61f47406cde897628b6
Yellokwest <saiete@student.dei.uc.pt>
2025-12-13 17:09:34 +0000

    ..

--- DEV/code_quality_toolkit/tests/plugins/duplication_checker/test_plugin_duplication.py
+++ DEV/code_quality_toolkit/tests/plugins/duplication_checker/test_plugin_duplication.py
@@ -8,7 +8,7 @@ from toolkit.plugins.duplicate_code_checker.plugin import Plugin
 
 
 class MockRulesConfig:
-    """Mock simples para compatibilidade com ToolkitConfig."""
+    """Mock simples para compatibilidade com ToolkitConfig.""" 
 
     max_line_length = 88
 
