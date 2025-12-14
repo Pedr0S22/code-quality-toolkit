65cecab8996480ac4299a0c818c0ef9648798db3
Gastonfdez <uo3028552@uniovi.es>
2025-12-14 19:19:22 +0000

    Añade plugin de dashboard de duplicación con tests unitarios

--- DEV/code_quality_toolkit/src/toolkit/plugins/duplication_plugin/duplication_dashboard.py
+++ DEV/code_quality_toolkit/src/toolkit/plugins/duplication_plugin/duplication_dashboard.py
@@ -1,6 +1,9 @@
-import json
+"""Módulo para generar el dashboard de duplicación."""
+
 
 class DuplicationDashboard:
+    """Clase encargada de renderizar reportes de duplicación."""
+
     def render_html(self, files_report):
         """
         Genera un reporte HTML simple a partir de los datos de duplicación.
@@ -12,17 +15,17 @@ class DuplicationDashboard:
             "<head><title>Reporte de Duplicación</title></head>",
             "<body>",
             "<h1>Resultados de Duplicación</h1>",
-            "<ul>"
+            "<ul>",
         ]
 
         # Recorremos cada archivo analizado
         for file_entry in files_report:
             file_name = file_entry.get("file", "Desconocido")
-            
+
             # Buscamos los resultados de los plugins dentro de este archivo
             plugins = file_entry.get("plugins", [])
             for plugin_result in plugins:
-                # Opcional: Filtramos para procesar solo nuestro plugin si fuera necesario
+                # Opcional: Filtramos para procesar solo nuestro plugin
                 # if plugin_result.get("plugin") != "DuplicationPlugin": continue
 
                 issues = plugin_result.get("results", [])
@@ -30,7 +33,7 @@ class DuplicationDashboard:
                     severity = issue.get("severity", "info")
                     message = issue.get("message", "")
                     line = issue.get("line", "?")
-                    
+
                     # Creamos el elemento de lista para el HTML
                     html_lines.append(
                         f"<li class='{severity}'>[{file_name}:{line}] {message}</li>"
@@ -39,13 +42,11 @@ class DuplicationDashboard:
         html_lines.append("</ul>")
         html_lines.append("</body>")
         html_lines.append("</html>")
-        
+
         return "\n".join(html_lines)
 
     def gen_dashboard(self, data, output_filename):
-        """
-        Genera el HTML y lo guarda en el archivo especificado.
-        """
+        """Genera el HTML y lo guarda en el archivo especificado."""
         html_content = self.render_html(data)
         with open(output_filename, "w", encoding="utf-8") as f:
             f.write(html_content)
